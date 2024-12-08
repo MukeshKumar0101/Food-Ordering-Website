9cce00ab155711f636e9b98743aa37360f21bb89
Mukesh Kumar <144682316+MukeshKumar0101@users.noreply.github.com>
2024-12-08 17:14:51 +0530

    added bg image to login form

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -7,6 +7,7 @@ import {
   signInWithEmailAndPassword,
 } from "firebase/auth";
 import { auth } from "../utils/firebase";
+import loginbg from "../assets/images/loginbg.jpg";
 
 function LoginForm() {
   const [isSignInForm, setIsSignInForm] = useState(true);
@@ -67,56 +68,53 @@ function LoginForm() {
 
   return (
     <div className="relative">
-      <img
-        className="w-full h-screen"
-        src="https://t3.ftcdn.net/jpg/05/89/70/46/360_F_589704609_b84XoVDaeopctL2Iz0lG4IQT86Dzm7xz.jpg"
-        alt=""
-      />
+      <img className="w-full h-screen" src={loginbg} alt="" />
       <div className="flex justify-center">
+        <form
+          onSubmit={(e) => e.preventDefault()}
+          className="absolute w-full top-[200px] bg-black/70 max-w-[500px] mx-auto py-10 px-10">
+          <h2 className="text-2xl font-bold text-white text-center pb-2">
+            {isSignInForm ? "Sign In" : "Sign Up"}
+          </h2>
+          <div className="flex flex-col">
+            {!isSignInForm && (
+              <input
+                type="text"
+                placeholder="Full Name"
+                className="p-3 m-2 px-2 rounded-md"
+              />
+            )}
 
-      <form
-        onSubmit={(e) => e.preventDefault()}
-        className="absolute w-full top-[200px] bg-black/70 max-w-[500px] mx-auto py-10 px-10">
-        <h2 className="text-2xl font-bold text-white text-center pb-2">
-          {isSignInForm ? "Sign In" : "Sign Up"}
-        </h2>
-        <div className="flex flex-col">
-          {!isSignInForm && (
             <input
+              ref={email}
               type="text"
-              placeholder="Full Name"
+              placeholder="Email Address"
               className="p-3 m-2 px-2 rounded-md"
             />
-          )}
-
-          <input
-            ref={email}
-            type="text"
-            placeholder="Email Address"
-            className="p-3 m-2 px-2 rounded-md"
-          />
-          {/* <p className="text-red-600 text-sm font-semibold px-3">{errorMsg}</p> */}
-          <input
-            ref={password}
-            type="password"
-            placeholder="Password"
-            className="p-3 m-2 px-2 rounded-md"
-          />
-          <p className="text-red-600 text-sm font-semibold px-3">{errorMsg}</p>
-          <button
-            onClick={handleButtonClick}
-            className="p-3 m-2 bg-[#E50914] hover:bg-[#8d1f25] transition-all text-white rounded-md">
-            {isSignInForm ? "Sign In" : "Sign Up"}
-          </button>
-        </div>
-        <p
-          className="p-3 text-sm font-bold cursor-pointer text-white"
-          onClick={toggleSignInForm}>
-          {isSignInForm
-            ? "New to Foody ? Sing Up Now"
-            : "Already registered ? Sign In"}
-        </p>
-      </form>
+            {/* <p className="text-red-600 text-sm font-semibold px-3">{errorMsg}</p> */}
+            <input
+              ref={password}
+              type="password"
+              placeholder="Password"
+              className="p-3 m-2 px-2 rounded-md"
+            />
+            <p className="text-red-600 text-sm font-semibold px-3">
+              {errorMsg}
+            </p>
+            <button
+              onClick={handleButtonClick}
+              className="p-3 m-2 bg-[#E50914] hover:bg-[#8d1f25] transition-all text-white rounded-md">
+              {isSignInForm ? "Sign In" : "Sign Up"}
+            </button>
+          </div>
+          <p
+            className="p-3 text-sm font-bold cursor-pointer text-white"
+            onClick={toggleSignInForm}>
+            {isSignInForm
+              ? "New to Foody ? Sing Up Now"
+              : "Already registered ? Sign In"}
+          </p>
+        </form>
       </div>
     </div>
   );
